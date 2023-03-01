78f5fbe6f3f7b432e06f4c8b84982ec97d49cb3f
WalanGun <ryw36237@gmail.com>
2023-03-01 14:36:05 +0700

    refact(mongodb): update cred

--- adapters/mongodb.py
+++ adapters/mongodb.py
@@ -3,6 +3,6 @@ from pymongo import MongoClient
 
 def get_mongo_client():
     mongo_client = MongoClient(
-        "mongodb+srv://trek:trek123bkk@trekcluster.gpygzfw.mongodb.net/?retryWrites=true&w=majority"
+        "mongodb+srv://trek:trekBKK123@trekcluster.gpygzfw.mongodb.net/?retryWrites=true&w=majority"
     )
     return mongo_client
