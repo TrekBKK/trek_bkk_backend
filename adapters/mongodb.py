928747e725d8f432459c3d4dc143db424accbc79
WalanGun <ryw36237@gmail.com>
2023-03-27 15:30:12 +0700

    [fix] move mongodb auth to .env

--- adapters/mongodb.py
+++ adapters/mongodb.py
@@ -1,8 +1,13 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
+load_dotenv()
+MONGO_USER_NAME = os.getenv('MONGO_USER_NAME')
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
 
 def get_mongo_client():
     mongo_client = MongoClient(
-        "mongodb+srv://trek:trekBKK123@trekcluster.gpygzfw.mongodb.net/?retryWrites=true&w=majority"
+        f"mongodb+srv://{MONGO_USER_NAME}:{MONGO_PASSWORD}@trekcluster.gpygzfw.mongodb.net/?retryWrites=true&w=majority"
     )
     return mongo_client
